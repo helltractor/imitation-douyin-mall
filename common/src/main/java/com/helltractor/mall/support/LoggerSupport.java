66dddaf87f0bfb1493f8d634ef51d3166396c4d7
helltractor <1664309610@qq.com>
2024-11-04 21:11:03 +0800

    fix: fix project dir framework

--- common/src/main/java/com/helltractor/mall/support/LoggerSupport.java
+++ common/src/main/java/com/helltractor/mall/support/LoggerSupport.java
@@ -3,8 +3,11 @@ package com.helltractor.mall.support;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * LoggerSupport
+ */
 public abstract class LoggerSupport {
     
     protected final static Logger logger = LoggerFactory.getLogger(LoggerSupport.class);
-    
+
 }
