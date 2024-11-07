1564f0cf7b5fbde8ada1e6175140588b74043de0
helltractor <1664309610@qq.com>
2024-11-07 20:53:12 +0800

    chore: add consul dependencies

--- server/src/main/java/com/helltractor/mall/service/CheckoutService.java
+++ server/src/main/java/com/helltractor/mall/service/CheckoutService.java
@@ -1,8 +1,8 @@
 package com.helltractor.mall.service;
 
-import com.helltractor.mall.proto.cart.CartServiceGrpc;
+import com.helltractor.mall.proto.checkout.CheckoutServiceGrpc;
 import net.devh.boot.grpc.server.service.GrpcService;
 
 @GrpcService
-public class CheckoutService extends CartServiceGrpc.CartServiceImplBase {
+public class CheckoutService extends CheckoutServiceGrpc.CheckoutServiceImplBase {
 }
--- server/src/main/java/com/helltractor/mall/service/OrderService.java
+++ server/src/main/java/com/helltractor/mall/service/OrderService.java
@@ -1,8 +1,20 @@
 package com.helltractor.mall.service;
 
-import com.helltractor.mall.proto.order.OrderServiceGrpc;
+import com.helltractor.mall.proto.order.*;
 import net.devh.boot.grpc.server.service.GrpcService;
 
 @GrpcService
 public class OrderService extends OrderServiceGrpc.OrderServiceImplBase {
+    
+    public PlaceOrderResp placeOrder(PlaceOrderReq request) {
+        return PlaceOrderResp.newBuilder().build();
+    }
+    
+    public ListOrderResp listOrder(ListOrderReq request) {
+        return ListOrderResp.newBuilder().build();
+    }
+    
+    public MarkOrderPaidResp markOrderPaid(MarkOrderPaidReq request) {
+        return MarkOrderPaidResp.newBuilder().build();
+    }
 }
