ddbd2ecd0061d5829a2ae3a225e21a8719fd790f
helltractor <1664309610@qq.com>
2024-11-11 21:42:10 +0800

    feat: add sentinel and sentinel-grpc dependencies

---
+++ server/src/main/java/com/helltractor/mall/config/SentinelRuleConfiguration.java
@@ -0,0 +1,22 @@
+package com.helltractor.mall.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SentinelRuleConfiguration {
+    
+    private static final Logger logger = LoggerFactory.getLogger(SentinelRuleConfiguration.class);
+    
+    @PostConstruct
+    public void init() {
+        logger.info("Load Sentinel Rules start!");
+        // TODO: Load Sentinel Rules
+        logger.info("Load Sentinel Rules end!");
+    }
+    
+    
+}
