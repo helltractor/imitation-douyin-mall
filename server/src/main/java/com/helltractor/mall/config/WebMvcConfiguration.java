1c96b8d6d3235611f34b8ab00b14afc50a0c01a4
helltractor <1664309610@qq.com>
2024-12-23 23:20:57 +0800

    chore(del): delete useless code

--- server/src/main/java/com/helltractor/mall/config/WebMvcConfiguration.java
+++
@@ -1,7 +0,0 @@
-package com.helltractor.mall.config;
-
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-
-public class WebMvcConfiguration extends WebMvcConfigurationSupport {
-
-}
